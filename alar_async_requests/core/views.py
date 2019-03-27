c5b08ae75fbd0afc39eafebd57241453dfc5375d
Ilya Peterov <ipeterov1@gmail.com>
2019-03-28 00:45:36 +0300

    Made url_load function ignore source files missing, timeouts and invalid data

--- alar_async_requests/core/views.py
+++ alar_async_requests/core/views.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import json
+from urllib.error import URLError
 from urllib.request import urlopen
 
 from django.conf import settings
@@ -8,17 +9,29 @@ from django.http import JsonResponse
 
 def index(request):
     def load_url(url, timeout):
-        with urlopen(url, timeout=timeout) as conn:
-            return json.loads(conn.read())
+        try:
+            response = urlopen(url, timeout=timeout)
+        except URLError:
+            return []
+
+        if response.status != 200:
+            return []
+
+        try:
+            data = json.loads(response.read())
+        except json.JSONDecodeError:
+            return []
+
+        return data
 
     data = []
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        futures = [executor.submit(load_url, url, 2) for url in settings.DATA_URLS]
+        futures = [executor.submit(load_url, url, timeout=2) for url in settings.DATA_URLS]
 
         for complete_future in concurrent.futures.as_completed(futures):
             data.extend(complete_future.result())
 
-    data.sort(key=lambda x: x['id'])
+    data.sort(key=lambda entry: entry['id'])
 
     return JsonResponse(data, safe=False)
